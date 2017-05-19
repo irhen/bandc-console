f3bb278fb5d78f35230237ed7c4cf22c7655915d
irhen <irhen@users.noreply.github.com>
2017-05-19 17:33:26 +0300

    Update bandc.rb
    changed some methods and variable names, should be more readable now

--- bandc.rb
+++ bandc.rb
@@ -35,8 +35,8 @@ end
 
 def gen_set(range, mult)
   result = Array(range)
-  mult.each do |k, v| 
-    result.concat(Array(0..(v - 2)).fill(k))
+  mult.each do |key, value| 
+    result.concat(Array(0..(value - 2)).fill(key))
   end
   result.sort
 end
@@ -52,49 +52,51 @@ def gen_mult(ranges, coeffs)
   result
 end
 
-def choosing_game
+def game_mode
   puts "Select classic or advanced game mode. For classic press 'c', for advanced press 'a'."
   input = gets.chomp.downcase
   yield input
 end
 
-def continue
+def will_play_again
   puts "Do you want to play again? Press 'y' to continue or any other key to exit."
   input = gets.chomp.downcase
   yield input
 end
 
-def classic_check(variable)
+def is_wrong(guess, number_of_digits)
   result = false
-  if variable.size != 4
+  if guess.length != number_of_digits
     puts "Please, try again. Your guess must contain exactly 4 digits!"
     result = true
   end
         
-  if variable.uniq.length != 4
+  if guess.uniq.length != number_of_digits
     puts "Please, try again. Your guess must contain 4 unique digits!"
     result = true
   end
     
-  if variable.include?(0)
+  if guess.include?(0) && number_of_digits == 4
     puts "Please, try again. You cannot use zero in your guess!"
     result = true
   end
   result
 end
 
-def advanced_check(variable)
-  result = false
-  if variable.size != 6
-    puts "Please, try again. Your guess must contain exactly 6 digits!"
-    result = true
+def how_gorgeous_is_user(score)
+  if score.length != 1
+    puts "You're gorgeous! It took you #{score.length} guesses!"
+  else
+    puts "You're gorgeous! It took you just one guess!"
   end
-        
-  if variable.uniq.length != 6
-    puts "Please, try again. Your guess must contain 6 unique digits!"
-    result = true
-  end
-  result
+end
+
+def guessing(secret_number, user_guess, game, type)
+  bulls_n_cows = secret_number&user_guess
+  bulls = game[type].map { |index| user_guess[index] == secret_number[index] }.select { |value| value }
+  cows = bulls_n_cows.length - bulls.length
+    
+  puts "#{user_guess.join("")} has #{bulls.length} bulls and #{cows} cows."
 end
 
 game = [[0, 1, 2, 3], [0, 1, 2, 3, 4, 5]]
@@ -104,45 +106,34 @@ game_type = ""
 
 choice = Proc.new { |input| game_type = input }
 
-choosing_game(&choice)
+game_mode(&choice)
 
-while game_type do
+while game_type
   if game_type == "c"  
     generated_mult = gen_mult(config[:classic][:ranges], config[:classic][:coeffs])
     generated_set = gen_set(1..9, generated_mult)
     secret_number = gen_num(generated_set, 4)
     puts "Input your guess using keyboard."
     
-    while user_guess != secret_number do
+    until user_guess == secret_number
       user_guess = gets.chomp.split("").map { |el| el.to_i }
       
-      if classic_check(user_guess) 
-        next
-      end
+      next if is_wrong(user_guess, 4) 
       
       score << user_guess
-      bulls_n_cows = secret_number&user_guess
-      bulls = game[0].map { |index| user_guess[index] == secret_number[index] }.select { |value| value }
-      cows = bulls_n_cows.length - bulls.length
-        
-      puts "#{user_guess.join("")} has #{bulls.length} bulls and #{cows} cows."
+      
+      guessing(secret_number, user_guess, game, 0)
     end
     
-    if score.size != 1
-      puts "You're gorgeous! It took you #{score.size} guesses!"
-    else
-      puts "You're gorgeous! It took you just one guess!"
-    end
+    how_gorgeous_is_user(score)
     
     score = []
     
-    continue(&choice)
+    will_play_again(&choice)
     
-    if game_type != "y"
-      break
-    end
+    break unless game_type == "y"
     
-    choosing_game(&choice)
+    game_mode(&choice)
   
   elsif game_type == "a"
     generated_mult = gen_mult(config[:advanced][:ranges], config[:advanced][:coeffs])
@@ -150,39 +141,28 @@ while game_type do
     secret_number = gen_num(generated_set, 6)
     puts "Input your guess using keyboard."
     
-    while user_guess != secret_number do
+    until user_guess == secret_number
       user_guess = gets.chomp.split("").map { |el| el.to_i }
 
-      if advanced_check(user_guess) 
-        next
-      end
+      next if is_wrong(user_guess, 6) 
       
       score << user_guess            
-      bulls_n_cows = secret_number&user_guess
-      bulls = game[1].map { |index| user_guess[index] == secret_number[index] }.select { |value| value }
-      cows = bulls_n_cows.length - bulls.length
-       
-      puts "#{user_guess.join("")} has #{bulls.length} bulls and #{cows} cows."
+      
+      guessing(secret_number, user_guess, game, 1)
     end
     
-    if score.size != 1
-      puts "You're gorgeous! It took you #{score.size} guesses!"
-    else
-      puts "You're gorgeous! It took you just one guess!"
-    end
+    how_gorgeous_is_user(score)
     
     score = []
     
-    continue(&choice)
+    will_play_again(&choice)
     
-    if game_type != "y"
-      break
-    end
+    break unless game_type == "y"
     
-    choosing_game(&choice)
+    game_mode(&choice)
     
   else 
     puts "Didn’t quite catch that. What was it, again?"
-    choosing_game(&choice)
+    game_mode(&choice)
   end
 end
